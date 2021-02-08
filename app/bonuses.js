e45e8d3354a3e827044f8fdf3cd3371f9f95d6cf
Nathan Quam <Nathan.M.Quam@gmail.com>
2021-02-08 15:24:21 -0700

    Bonuses

--- app/bonuses.js
+++ app/bonuses.js
@@ -2,5 +2,18 @@
 let str = "team number 2"
 
 function camelCase(str) {
+   let newString = str;
 
+   for(let i = 1; i < newString.length; i++) {
+      if(newString[i - 1] == " ") {
+         newString = newCapitalizer(newString, i);
+      }
+   }
+   newString = newString.split(" ").join("");
+
+   return newString;
+}
+
+function newCapitalizer(str, position) {
+   return str.substr(0, position) + str.charAt(position).toUpperCase() + str.substr(position + 1, str.length-1);
 }
